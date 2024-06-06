1a7b7fb9fd7bff8ccd5ee2f773b6d5fc300d1579
talesh <tales.hein@hotmail.com>
2024-06-05 23:48:36 -0300

    :sparkles: handler da home

--- internal/router/router.go
+++ internal/router/router.go
@@ -9,5 +9,8 @@ import (
 func NewRouter() *mux.Router {
 	r := mux.NewRouter()
 	r.HandleFunc("/", handler.HomeHandler).Methods("GET")
+	r.HandleFunc("/repo", handler.RepoHandler).Methods("GET")
+	r.HandleFunc("/route/{routeId}", handler.RouteHandler).Methods("GET")
+	r.HandleFunc("/creation", handler.CreationHandler).Methods("GET")
 	return r
 }
